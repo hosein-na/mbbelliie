542db3f9779aa6cfcd9fb15138837390a3a2fe53
mbbelliie <nhsyn00021@gmail.com>
2025-07-22 17:10:34 +0330

    edit 2.6

--- src/portfolio.js
+++ src/portfolio.js
@@ -22,26 +22,14 @@ const illustration = {
 const greeting = {
   username: "موکب بی بی علیّه خاتون(س)",
   title: "سلام خدمت همه زوار علی بن موسی الرضا(ع)",
-  subTitle: emoji(
-    "التماس دعا از همگی"
-  ),
+  subTitle: emoji("التماس دعا از همگی"),
   resumeLink:
-    '<div id="11451485907"><script type="text/JavaScript"'
-  displayGreeting: true // Set false to hide this section, defaults to true
+    '<div id="11451485907"><script type="text/JavaScript" src="https://www.aparat.com/embed/wk4IF?data[rnddiv]=11451485907&data[responsive]=yes"></script></div>',
+  displayGreeting: true
 };
-
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
-  // Instagram, Twitter and Kaggle are also supported in the links!
-  // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
 };
 

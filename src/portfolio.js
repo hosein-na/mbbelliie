ef1dabd3082ea5fddb98ffd3901f98c360f55a5e
mbbelliie <nhsyn00021@gmail.com>
2025-07-22 14:03:12 +0330

    edit 1

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,10 +20,10 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "موکب بی بی علیّه خاتون(س)",
+  title: "سلام خدمت همه زوار علی بن موسی الرضا(ع)",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "التماس دعا از همگی"
   ),
   resumeLink:
     "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
